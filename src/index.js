fe99067425563d04d162d7d848bf5411b8d188e6
Andrew Johnston <asjohnston@alaska.edu>
2019-01-22 20:48:55 +0000

    initial commit

---
+++ src/index.js
@@ -0,0 +1,190 @@
+'use strict';
+
+
+const EarthdataLoginClient = require('./lib/EarthdataLogin');
+const jwt = require('jsonwebtoken');
+const urljoin = require('url-join');
+const SecretsManager = require('aws-sdk/clients/secretsmanager');
+const secretsManager = new SecretsManager({region: 'us-east-1'});
+
+
+async function getConfig() {
+  const request = secretsManager.getSecretValue({SecretId: 'grfn-cloudfront-secret'});
+  const result = await request.promise();
+  return JSON.parse(result.SecretString);
+}
+
+const configPromise = getConfig();
+
+
+function redirectResponse(location) {
+  return {
+    status: '307',
+    statusDescription: 'Temporary Redirect',
+    headers: {
+      location: [{
+        key: 'Location',
+        value: location
+      }]
+    }
+  };
+}
+
+
+function parseCookies(headers) {
+  let parsedCookies = {};
+  if (headers.cookie) {
+    for (let i = 0; i < headers.cookie.length; i++) {
+      headers.cookie[i].value.split(';').forEach((cookie) => {
+        if (cookie) {
+          const parts = cookie.split('=');
+          parsedCookies[parts[0].trim()] = parts[1].trim();
+        }
+      });
+    }
+  }
+  return parsedCookies;
+}
+
+
+function parseQueryString(queryString) {
+  let parsedParameters = {};
+  if (queryString) {
+    queryString.split('&').forEach((parameter) => {
+      if (parameter) {
+        const parts = parameter.split('=');
+        parsedParameters[decodeURIComponent(parts[0].trim())] = decodeURIComponent(parts[1].trim());
+      }
+    });
+  }
+  return parsedParameters;
+}
+
+
+function isRedirectRequest(request) {
+  return request.uri === '/redirect';
+}
+
+
+function getAccessTokenFromRequest(request) {
+  const parsedCookies = parseCookies(request.headers);
+  if (parsedCookies && parsedCookies['accessToken']) {
+    return parsedCookies['accessToken'];
+  }
+  return null;
+}
+
+
+async function handleRedirectRequest(params = {}) {
+  const {
+    authClient,
+    distributionUrl,
+    privateKey,
+    request
+  } = params;
+
+  const queryStringParameters = parseQueryString(request.querystring);
+  const accessTokenResponse = await authClient.getAccessToken(queryStringParameters.code);
+
+  const payload = {
+    username: accessTokenResponse.username
+  };
+
+  const options = {
+    algorithm: 'RS256',
+    expiresIn: 60
+  };
+
+  const accessToken = jwt.sign(payload, privateKey, options);
+
+  return {
+    status: '307',
+    statusDescription: 'Temporary Redirect',
+    headers: {
+      'location': [{
+        key: 'Location',
+        value: urljoin(distributionUrl, queryStringParameters.state)
+      }],
+      'set-cookie': [{
+        key: 'Set-Cookie',
+        value: `accessToken=${accessToken}`
+      }]
+    }
+  };
+}
+
+
+async function handleFileRequest(params = {}) {
+  const {
+    authClient,
+    publicKey,
+    request,
+  } = params;
+
+  const redirectToGetAuthorizationCode = redirectResponse(authClient.getAuthorizationUrl(request.uri));
+  const accessToken = getAccessTokenFromRequest(request);
+
+  if (!accessToken) {
+    console.log('no access token');
+    return redirectToGetAuthorizationCode;
+  }
+
+  try {
+    const payload = jwt.verify(accessToken, publicKey);
+    console.log(payload['username']);
+  } catch(err) {
+    console.log('bad token');
+    return redirectToGetAuthorizationCode;
+  }
+
+  return request;
+}
+
+
+async function handleRequest(params = {}) {
+  const {
+    authClient,
+    distributionUrl,
+    publicKey,
+    privateKey,
+    request
+  } = params;
+
+  if (isRedirectRequest(request)) {
+    return await handleRedirectRequest({
+      authClient,
+      distributionUrl,
+      privateKey,
+      request
+    });
+  }
+
+  return await handleFileRequest({
+    authClient,
+    publicKey,
+    request
+  });
+}
+
+
+exports.handler = async function(event, context) {
+  const request = event.Records[0].cf.request;
+  const config = await configPromise;
+
+  const earthdataLoginClient = new EarthdataLoginClient({
+    clientId: config.ursClientId,
+    clientPassword: config.ursClientPassword,
+    earthdataLoginUrl: config.ursHostname,
+    redirectUri: `https://${config.cloudfrontDomain}/redirect`
+  });
+
+  const response = await handleRequest({
+    authClient: earthdataLoginClient,
+    distributionUrl: `https://${config.cloudfrontDomain}/`,
+    publicKey: config.publicKey,
+    privateKey: config.privateKey,
+    request: request
+  });
+
+  return response;
+};
