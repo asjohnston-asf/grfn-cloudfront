f5ef891882091f7cc6d275c3c00c97eb29d74f0b
Andrew Johnston <asjohnston@alaska.edu>
2019-08-04 17:31:45 +0000

    use pip3

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-pip install -t src/ -r requirements.txt
+pip3 install -t src/ -r requirements.txt
 sed "s/REPLACE/$(date +%s)/" cloudformation.yaml > cf.yaml
 aws cloudformation package --template-file cf.yaml --s3-bucket asjohnston-dev --s3-prefix cloudformation/ --output-template-file cf.yaml
 aws cloudformation deploy --template-file cf.yaml --stack-name asjohnston-cloudfront-dev --capabilities CAPABILITY_NAMED_IAM
