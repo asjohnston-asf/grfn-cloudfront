b13d2222977c4239f95e9ac475b07d1f8daacee2
Andrew Johnston <asjohnston@alaska.edu>
2019-09-01 20:37:35 +0000

    cleanup

--- src/main.py
+++ src/main.py
@@ -9,28 +9,28 @@ import jwt
 import boto3
 from requests_oauthlib import OAuth2Session
 
-secrets_manager = boto3.client('secretsmanager', region_name='us-east-1')
-s3 = boto3.client('s3')
+SECRETS_MANAGER = boto3.client('secretsmanager', region_name='us-east-1')
+S3 = boto3.client('s3')
 
-config = None
-urs = None
-aws_ip_blocks = None
+CONFIG = None
+URS = None
+AWS_IP_BLOCKS = None
 
 
 def setup(secret_name):
-    global config
-    config = get_config(secret_name)
+    global CONFIG
+    CONFIG = get_config(secret_name)
 
-    global urs
-    redirect_uri = 'https://' + config['cloudfrontDomain'] + '/oauth'
-    urs = OAuth2Session(config['ursClientId'], redirect_uri=redirect_uri)
+    global URS
+    redirect_uri = 'https://' + CONFIG['cloudfrontDomain'] + '/oauth'
+    URS = OAuth2Session(CONFIG['ursClientId'], redirect_uri=redirect_uri)
 
-    global aws_ip_blocks
-    aws_ip_blocks = get_aws_ip_blocks()
+    global AWS_IP_BLOCKS
+    AWS_IP_BLOCKS = get_aws_ip_blocks()
 
 
 def get_config(secret_name):
-    secret = secrets_manager.get_secret_value(SecretId=secret_name)
+    secret = SECRETS_MANAGER.get_secret_value(SecretId=secret_name)
     config = loads(secret['SecretString'])
     return config
 
@@ -57,41 +57,49 @@ def redirect_response(url):
     }
 
 
+def browser_user_agent(headers):
+    if not headers.get('user-agent'):
+        return False
+    for header in headers['user-agent']:
+        if not header['value'].startswith('Mozilla'):
+            return False
+    return True
+
+
 def redirect_to_login(request):
-    authorization_base_url = config['ursHostname'] + '/oauth/authorize'
-    authorization_url, state = urs.authorization_url(authorization_base_url, state=request['uri'])
-    if 'user-agent' not in request['headers'] or not request['headers']['user-agent'][0]['value'].startswith('Mozilla'):
+    authorization_base_url = CONFIG['ursHostname'] + '/oauth/authorize'
+    authorization_url, state = URS.authorization_url(authorization_base_url, state=request['uri'])
+    if not browser_user_agent(request['headers']):
         authorization_url += '&app_type=401'
-    response = redirect_response(authorization_url)
-    return response
+    return redirect_response(authorization_url)
 
 
 def set_cookie_header(token):
     cookie = SimpleCookie()
     cookie['session-token'] = token
-    cookie['session-token']['expires'] = config['sessionDurationInSeconds']
-    set_cookie_header = [{
+    cookie['session-token']['expires'] = CONFIG['sessionDurationInSeconds']
+    header = [{
         'key': 'Set-Cookie',
         'value': cookie.output(header='')
     }]
-    return set_cookie_header
+    return header
 
 
 def get_session_token(request):
     parms = parse_qs(request['querystring'])
-    token_uri = config['ursHostname'] + '/oauth/token'
-    token = urs.fetch_token(token_uri, code=parms['code'][0], client_secret=config['ursClientPassword'])
-    user_profile_uri = config['ursHostname'] + token['endpoint']
+    token_uri = CONFIG['ursHostname'] + '/oauth/token'
+    token = URS.fetch_token(token_uri, code=parms['code'][0], client_secret=CONFIG['ursClientPassword'])
+    user_profile_uri = CONFIG['ursHostname'] + token['endpoint']
     response = requests.get(user_profile_uri, headers={'Authorization': token['token_type'] + ' ' + token['access_token']})
     response.raise_for_status()
     user = response.json()
-    expiration_time = datetime.utcnow() + timedelta(seconds=config['sessionDurationInSeconds'])
+    expiration_time = datetime.utcnow() + timedelta(seconds=CONFIG['sessionDurationInSeconds'])
     payload = {
         'user_id': user['uid'],
         'exp': expiration_time.strftime('%s'),
     }
-    session_token = jwt.encode(payload, config['secretKey'], 'HS256')
-    redirect_url = 'https://' + config['cloudfrontDomain'] + parms['state'][0]
+    session_token = jwt.encode(payload, CONFIG['secretKey'], 'HS256')
+    redirect_url = 'https://' + CONFIG['cloudfrontDomain'] + parms['state'][0]
     response = redirect_response(redirect_url)
     response['headers']['set-cookie'] = set_cookie_header(session_token.decode('utf-8'))
     return response
@@ -99,17 +107,17 @@ def get_session_token(request):
 
 def is_aws_address(ip):
     addr = ip_address(ip)
-    for block in aws_ip_blocks:
+    for block in AWS_IP_BLOCKS:
         if addr in block:
             return True
     return False
 
 
 def redirect_to_s3(key, user_id):
-    signed_url = s3.generate_presigned_url(
+    signed_url = S3.generate_presigned_url(
         ClientMethod='get_object',
         Params={
-            'Bucket': config['bucket'],
+            'Bucket': CONFIG['bucket'],
             'Key': key,
         },
         ExpiresIn=60,
@@ -122,8 +130,9 @@ def lambda_handler(event, context):
     request = event['Records'][0]['cf']['request']
     headers = request['headers']
 
-    if not config:
-        setup(context.function_name.split('.')[-1])
+    if not CONFIG:
+        secret_name = context.function_name.split('.')[-1]
+        setup(secret_name)
 
     if request['uri'] == '/oauth':
         return get_session_token(request)
@@ -137,7 +146,7 @@ def lambda_handler(event, context):
 
     token = cookie['session-token'].value
     try:
-        payload = jwt.decode(token, config['secretKey'], algorithms=['HS256'])
+        payload = jwt.decode(token, CONFIG['secretKey'], algorithms=['HS256'])
     except (jwt.DecodeError, jwt.ExpiredSignatureError):
         return redirect_to_login(request)
 
