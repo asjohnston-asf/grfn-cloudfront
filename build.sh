976c7c79a0807d4fac078b982293bd2ac8ea3a39
EC2 Default User <ec2-user@ip-172-31-45-101.ec2.internal>
2020-02-01 21:22:17 +0000

    deployable version

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
 pip3 install -t viewer-request/src/ -r viewer-request/requirements.txt
-pip3 install -t log-parse/src/ -r log-parse/requirements.txt
 find . -name "__pycache__" | xargs rm -R
 find . -name "*dist-info" | xargs rm -R
 sed "s/REPLACE/$(date +%s)/" cloudformation.yaml > cf.yaml
 date +%s > viewer-request/src/dummy-file
 aws cloudformation package --template-file cf.yaml --s3-bucket asjohnston-dev --s3-prefix cloudformation --output-template-file cf.yaml
-aws cloudformation deploy --template-file cf.yaml --stack-name asjohnston-cf-dev --capabilities CAPABILITY_NAMED_IAM
+#aws cloudformation deploy --template-file cf.yaml --stack-name asjohnston-cf-dev --capabilities CAPABILITY_NAMED_IAM
--- viewer-request/src/main.py
+++ viewer-request/src/main.py
@@ -1,40 +1,18 @@
 from json import loads
-from urllib.parse import parse_qs
-from datetime import datetime, timedelta
 from http.cookies import SimpleCookie
+from urllib.parse import urlunsplit, quote_plus
 
 import jwt
 from boto3 import client
-from requests_oauthlib import OAuth2Session
-from oauthlib.oauth2 import InvalidGrantError
 
 SECRETS_MANAGER = client('secretsmanager', region_name='us-east-1')
-
 CONFIG = None
-URS = None
-AWS_IP_BLOCKS = None
 
 
-def setup(secret_name, cloudfront_domain_name):
-    global CONFIG
-    CONFIG = get_config(secret_name)
-
-    global URS
-    redirect_uri = 'https://' + cloudfront_domain_name + '/oauth'
-    URS = OAuth2Session(CONFIG['ursClientId'], redirect_uri=redirect_uri)
-
-
-def get_config(secret_name):
+def load_config(secret_name):
     secret = SECRETS_MANAGER.get_secret_value(SecretId=secret_name)
-    config = loads(secret['SecretString'])
-    return config
-
-
-def client_error_response():
-    return {
-        'status': '400',
-        'statusDescription': 'Bad Request',
-    }
+    global CONFIG
+    CONFIG = loads(secret['SecretString'])
 
 
 def redirect_response(url):
@@ -60,47 +38,11 @@ def browser_user_agent(headers):
 
 
 def redirect_to_login(request):
-    authorization_base_url = CONFIG['ursHostname'] + '/oauth/authorize'
-    authorization_url, state = URS.authorization_url(authorization_base_url, state=request['uri'])
+    invoked_url = urlunsplit(['https', CONFIG['domainName'], request['uri'], '', ''])
+    redirect_url = CONFIG['authUrl'] + '&state=' + quote_plus(invoked_url)
     if not browser_user_agent(request['headers']):
-        authorization_url += '&app_type=401'
-    return redirect_response(authorization_url)
-
-
-def set_cookie_header(token):
-    cookie = SimpleCookie()
-    cookie['session-token'] = token
-    cookie['session-token']['expires'] = CONFIG['sessionDurationInSeconds']
-    header = [{
-        'key': 'Set-Cookie',
-        'value': cookie.output(header='')
-    }]
-    return header
-
-
-def get_session_token(request):
-    parms = parse_qs(request['querystring'])
-    if not parms.get('code'):
-        return client_error_response()
-
-    token_uri = CONFIG['ursHostname'] + '/oauth/token'
-    try:
-        urs_token = URS.fetch_token(token_uri, code=parms['code'][0], client_secret=CONFIG['ursClientPassword'])
-    except InvalidGrantError:
-        return client_error_response()
-
-    user_id = urs_token['endpoint'].split('/')[-1]
-    expiration_time = datetime.utcnow() + timedelta(seconds=CONFIG['sessionDurationInSeconds'])
-    payload = {
-        'user_id': user_id,
-        'exp': expiration_time.strftime('%s'),
-    }
-    session_token = jwt.encode(payload, CONFIG['secretKey'], 'HS256')
-
-    redirect_url = parms.get('state', ['/'])[0]
-    response = redirect_response(redirect_url)
-    response['headers']['set-cookie'] = set_cookie_header(session_token.decode('utf-8'))
-    return response
+        redirect_url += '&app_type=401'
+    return redirect_response(redirect_url)
 
 
 def lambda_handler(event, context):
@@ -109,22 +51,18 @@ def lambda_handler(event, context):
 
     if not CONFIG:
         secret_name = context.function_name.split('.')[-1]
-        cloudfront_domain_name = event['Records'][0]['cf']['config']['distributionDomainName']
-        setup(secret_name, cloudfront_domain_name)
-
-    if request['uri'] == '/oauth':
-        return get_session_token(request)
+        load_config(secret_name)
 
     if not headers.get('cookie'):
         return redirect_to_login(request)
 
     cookie = SimpleCookie(headers['cookie'][0]['value'])
-    if not cookie.get('session-token'):
+    if not cookie.get(CONFIG['cookieName']):
         return redirect_to_login(request)
 
-    token = cookie['session-token'].value
+    token = cookie[CONFIG['cookieName']].value
     try:
-        jwt.decode(token, CONFIG['secretKey'], algorithms=['HS256'])
+        jwt.decode(token, CONFIG['publicKey'])
     except (jwt.DecodeError, jwt.ExpiredSignatureError):
         return redirect_to_login(request)
 
